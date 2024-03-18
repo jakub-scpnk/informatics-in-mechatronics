208f7de920937a25adef475c63070e1a6a952f70
jakub-scpnk <jakubszczepaniak987@gmail.com>
2024-03-19 00:50:22 +0100

    First task finished

--- lab_1/src/Main.java
+++ lab_1/src/Main.java
@@ -1,15 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static double factorial(int baseNum) {
+        if (baseNum == 0 || baseNum == 1) {
+            return 1;
+        } else {
+            return baseNum * factorial(baseNum - 1);
         }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // create a Scanner object to read input
+
+        System.out.print("\n" + "Hello and welcome!");
+        System.out.print("""
+
+                This program calculates exponential function term based on Maclaurin series expansion.
+                """);
+        System.out.print("Enter value of x: ");
+
+        float x = scanner.nextFloat();
+        double ans = 1;
+        int iter = 1;
+        double errAbs;
+        double trueValue = Math.exp(x);
+        System.out.println("You entered: " + x);
+        System.out.println("True value: " + trueValue);
+
+        do {
+            double oldAns = ans;
+            ans = ans + Math.pow(x, iter) / factorial(iter);
+            iter += 1;
+            System.out.println("e^" + x + " (iteration: " + iter +") = " + ans);
+
+            errAbs = Math.abs((ans - oldAns) / ans) * 100;
+
+        } while (errAbs > 1);
+        scanner.close(); // closing Scanner object
     }
 }
