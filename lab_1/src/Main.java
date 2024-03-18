d0985c0d60118b26d883ace74b73ee4291a1f832
jakub-scpnk <jakubszczepaniak987@gmail.com>
2024-03-19 00:54:12 +0100

    Small refactorization.

--- lab_1/src/Main.java
+++ lab_1/src/Main.java
@@ -11,9 +11,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // create a Scanner object to read input
 
-        System.out.print("\n" + "Hello and welcome!");
         System.out.print("""
 
+                "Hello and welcome!"
+                
                 This program calculates exponential function term based on Maclaurin series expansion.
                 """);
         System.out.print("Enter value of x: ");
